4a72e7c6ec9a38a2bdba92c91c93b77ad431091f
Jorge79 <Jorge7.9@hotmail.com>
2024-01-25 11:44:42 -0300

    app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CheckoutsModule } from './checkouts/checkouts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {
+  Checkout,
+  CheckoutItem,
+  CheckoutProduct,
+} from './checkouts/entities/checkout.entity';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'nest',
+      entities: [Checkout, CheckoutProduct, CheckoutItem],
+      synchronize: true,
+      logging: true,
+    }),
+    CheckoutsModule,
+  ],
 })
 export class AppModule {}
